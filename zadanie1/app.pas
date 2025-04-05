8053dd0f74d062429eb41f1a4dc5cc6c96544038
kreciszj <jakub.krecisz@icloud.com>
2025-04-05 21:09:50 +0200

    feat: exercise 1 grade 3.5

--- zadanie1/app.pas
+++ zadanie1/app.pas
@@ -1,18 +1,50 @@
 
 Program app;
 
-procedure GenerateRandomNumbers;
+procedure GenerateRandomNumbers(var arr: array of integer);
 var
   i: integer;
 begin
   randomize;
-  for i := 1 to 50 do
+  for i := Low(arr) to High(arr) do
   begin
-    writeln('Random number ', i, ': ', random(101));
+    arr[i] := random(101);
   end;
 end;
 
+procedure BubbleSort(var arr: array of integer);
+var
+  i, j, temp: integer;
 begin
-  writeln('Generating 50 random numbers from 0 to 100...');
-  GenerateRandomNumbers;
+  for i := Low(arr) to High(arr) do
+  begin
+    for j := Low(arr) to High(arr) - 1 do
+    begin
+      if arr[j] > arr[j+1] then
+      begin
+        temp := arr[j];
+        arr[j] := arr[j+1];
+        arr[j+1] := temp;
+      end;
+    end;
+  end;
+end;
+
+var
+  numbers: array[1..50] of integer;
+  i: integer;
+begin
+  GenerateRandomNumbers(numbers);
+
+  writeln('Unsorted numbers:');
+  for i := 1 to 50 do
+    write(numbers[i], ' ');
+  writeln;
+
+  BubbleSort(numbers);
+
+  writeln('Sorted numbers:');
+  for i := 1 to 50 do
+    write(numbers[i], ' ');
+  writeln;
 end.
