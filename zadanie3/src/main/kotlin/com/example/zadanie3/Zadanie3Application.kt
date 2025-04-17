c315225ae7f0a26b420cf991f85434b027012a57
kreciszj <jakub.krecisz@icloud.com>
2025-04-17 19:15:27 +0200

    feat: exercise 3 grade 4.5

--- zadanie3/src/main/kotlin/com/example/zadanie3/Zadanie3Application.kt
+++ zadanie3/src/main/kotlin/com/example/zadanie3/Zadanie3Application.kt
@@ -1,10 +1,19 @@
 package com.example.zadanie3
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Zadanie3Application
+class Zadanie3Application : CommandLineRunner {
+	@Autowired
+	private lateinit var authService: AuthService
+
+	override fun run(vararg args: String?) {
+		println(">>> Zadanie3Application got AuthService instance: $authService")
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Zadanie3Application>(*args)
